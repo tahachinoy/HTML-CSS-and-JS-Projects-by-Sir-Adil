0d3a4b742e52424a5f5f2dc8a0efdc5d12503ae4
tahachinoy <tahachinoy@gmail.com>
2024-08-31 23:43:49 +0500

    Update script.js
    Added functionality to check email validation, check length validation and also password match validation

--- project 1 - Signup Form/script.js
+++ project 1 - Signup Form/script.js
@@ -29,16 +29,19 @@ function showSuccess(input) {
 
 //Function to check if email is valid
 
-function isValidEmail (email){
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, `Please provide a valid ${getFieldId(input)}`)
+    }
 }
-
 //Function to check if required fields have data
 
 function checkRequired(inputArray) {
-    inputArray.forEach(function(input) {
-        if ( input.value === '') {
+    inputArray.forEach(function (input) {
+        if (input.value === '') {
             //showError(input, input.id + 'is required'); ye ek tareeqa hai
             //showError(input, `${input.id} is required`); ye bhi ek tareeqa hai lekin is main username email sab ka first letter capital nahi ho raha, ek teesra tareeqa hai jis ke liye we will create a small function of getfieldid
             showError(input, `${getFieldId(input)} is required`)
@@ -48,11 +51,30 @@ function checkRequired(inputArray) {
     })
 
 }
+//Function to check length of input field
+
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldId(input)} needs to be atleast ${min} charachters`)
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldId(input)} needs to be less than ${max} charachters`)
+    } else {
+        showSuccess(input);
+    }
+}
+
+//Function to check if password and conffirm pasword match
+
+function checkPassword (input1, input2) {
+    if (input1.value !== input2.value) {
+        showError (input2, "Passwords don't match")
+    }
+}
 //Function to get the id of the input field
 
 function getFieldId(input) {
-   //return input.id.toUpperCase(); is se sab uppercase ho jaayein ge humain sab uppercase main nahi karnay
-   return input.id.charAt(0).toUpperCase() + input.id.slice(1); //input id slice 1 se basically hum first letter ko baaki letters se concatenate kar rahay hain 
+    //return input.id.toUpperCase(); is se sab uppercase ho jaayein ge humain sab uppercase main nahi karnay
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1); //input id slice 1 se basically hum first letter ko baaki letters se concatenate kar rahay hain 
 }
 
 //Event listeneres are usually added at the end of the page
@@ -62,5 +84,10 @@ form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 10);
+    checkLength(password, 6, 30);
+    checkEmail(email);
+    checkPassword(password, password2);
+
 
 });
